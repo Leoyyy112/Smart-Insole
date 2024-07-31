297d133eec9acf89cce201f3d11e1a596c817332
‘yang‘ <lyleo0110@gmail.com>
2024-07-31 14:50:50 +0100

    Initial Commit

---
+++ StepTrackerApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StepTrackerApp
+//
+//  Created by 刘阳 on 2024/7/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StepTrackerApp/StepTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepTrackerAppApp.swift
+//  StepTrackerApp
+//
+//  Created by 刘阳 on 2024/7/31.
+//
+
+import SwiftUI
+
+@main
+struct StepTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
